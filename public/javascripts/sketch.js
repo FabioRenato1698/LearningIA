c89ff3503c3dab11801d887adaeceb986a4f3101
Fabio Costa <fabiocosta@Air-de-Fabio.Home>
2020-05-19 00:00:16 +0100

    4 Entrega

--- public/javascripts/sketch.js
+++ public/javascripts/sketch.js
@@ -27,6 +27,7 @@ function setup() {
 
     // Create a population with a target phrase, mutation rate, and population max
     population = new Population(target, mutationRate, popmax);
+    $('#hillSearchRestart').click(displayInfo);
 }
 
 function draw() {
